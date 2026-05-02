e36f3e453005561f79c10822fe334b12bd400de6
Robert Turner <robert@redphasetech.com>
2026-05-02 15:10:52 +1200

    Reduce ringbuff size

--- components/vban_receiver/vban_receiver.h
+++ components/vban_receiver/vban_receiver.h
@@ -253,7 +253,7 @@ class VBANReceiver : public Component {
   bool playing_{false};
 
   // Fixed-capacity circular buffer (~1 s at 16 kHz 16-bit mono).
-  static constexpr size_t kRingCapacity = 3*32 * 1024;
+  static constexpr size_t kRingCapacity = 32 * 1024;
   std::vector<uint8_t> ring_;
   size_t write_idx_{0};
   size_t ring_size_{0};
